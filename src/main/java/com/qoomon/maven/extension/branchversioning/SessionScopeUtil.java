bfd29bc5da4a181e9034074dd4cb6f664b944ab1
Bengt Brodersen <bengt.brodersen@deposit-solutions.com>
2016-12-06 18:04:52 +0100

    refactor: remove intelliJ fix code

--- src/main/java/com/qoomon/maven/extension/branchversioning/SessionScopeUtil.java
+++ src/main/java/com/qoomon/maven/extension/branchversioning/SessionScopeUtil.java
@@ -1,7 +1,6 @@
 package com.qoomon.maven.extension.branchversioning;
 
 import com.google.inject.Key;
-import com.google.inject.OutOfScopeException;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.session.scope.internal.SessionScope;
 
@@ -11,10 +10,6 @@ import org.apache.maven.session.scope.internal.SessionScope;
 public class SessionScopeUtil {
 
     public static MavenSession getMavenSession(SessionScope sessionScope) {
-        try {
-            return sessionScope.scope(Key.get(MavenSession.class), null).get();
-        } catch (OutOfScopeException ex) {
-            return null;
-        }
+        return sessionScope.scope(Key.get(MavenSession.class), null).get();
     }
 }
