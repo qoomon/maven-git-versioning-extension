8e05e2d0bfad197520a6da60b126393611ceb746
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-10-14 23:13:40 +0200

    feat: add java9 compatibility

--- src/main/java/com/qoomon/maven/extension/branchversioning/config/model/BranchVersionDescription.java
+++ src/main/java/com/qoomon/maven/extension/branchversioning/config/model/BranchVersionDescription.java
@@ -1,24 +1,17 @@
 package com.qoomon.maven.extension.branchversioning.config.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+
+import org.simpleframework.xml.Element;
 
 /**
  * Created by qoomon on 26/11/2016.
  */
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 public class BranchVersionDescription {
 
-    @XmlElement(required = true, name = "pattern")
-    public String branchPattern;
+    @Element
+    public String pattern;
 
-    @XmlElement(required = true)
+    @Element
     public String versionFormat;
-
-    @XmlElement(required = false)
-    public String tagFormat;
 
 }
