8b9086c3f00652a1ad0e00fb291097d44e38f03e
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-10-31 18:13:48 +0100

    refactor

---
+++ src/main/java/me/qoomon/maven/extension/gitversioning/GitUtil.java
@@ -0,0 +1,76 @@
+package me.qoomon.maven.extension.gitversioning;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class GitUtil {
+
+    public static Status getStatus(Repository repository) {
+        try {
+            return Git.wrap(repository).status().call();
+        } catch (GitAPIException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Optional<String> getHeadBranch(Repository repository) throws IOException {
+
+        ObjectId head = repository.resolve(Constants.HEAD);
+        if (head == null) {
+            return Optional.of(Constants.MASTER);
+        }
+
+        if (ObjectId.isId(repository.getBranch())) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(repository.getBranch());
+    }
+
+    public static List<String> getHeadTags(Repository repository) throws IOException {
+
+        ObjectId head = repository.resolve(Constants.HEAD);
+        if (head == null) {
+            return Collections.emptyList();
+        }
+
+        return repository.getRefDatabase().getRefsByPrefix(Constants.R_TAGS).stream()
+                .map(ref -> {
+                    try {
+                        return repository.getRefDatabase().peel(ref);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .filter(ref -> {
+                    ObjectId objectId;
+                    if (ref.getPeeledObjectId() != null) {
+                        objectId = ref.getPeeledObjectId();
+                    } else {
+                        objectId = ref.getObjectId();
+                    }
+                    return objectId.equals(head);
+                })
+                .map(ref -> ref.getName().replaceFirst("^" + Constants.R_TAGS, ""))
+                .collect(Collectors.toList());
+    }
+
+    public static String getHeadCommit(Repository repository) throws IOException {
+
+        ObjectId head = repository.resolve(Constants.HEAD);
+        if (head == null) {
+            return "0000000000000000000000000000000000000000";
+        }
+        return head.getName();
+    }
+}
