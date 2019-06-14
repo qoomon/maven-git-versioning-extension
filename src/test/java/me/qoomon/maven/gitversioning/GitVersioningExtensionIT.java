b36c172c9e18ad565aeb1af6f11f53f88f114967
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-06-14 18:24:31 +0200

    test: seperate tests

--- src/test/java/me/qoomon/maven/gitversioning/GitVersioningExtensionIT.java
+++ src/test/java/me/qoomon/maven/gitversioning/GitVersioningExtensionIT.java
@@ -174,7 +174,6 @@ class GitVersioningExtensionIT {
         pomModel.setPackaging("pom");
         pomModel.addModule("api");
         pomModel.addModule("logic");
-        pomModel.addModule("duplicate-artifactid-different-groupid");
         pomModel.setDependencyManagement(new DependencyManagement() {{
             addDependency(new Dependency() {{
                 setGroupId("${project.groupId}");
@@ -212,16 +211,6 @@ class GitVersioningExtensionIT {
                 setVersion(pomModel.getVersion());
             }});
             setArtifactId("logic");
-        }});
-        Path duplicateArtifactidProjectDir = Files.createDirectories(projectDir.resolve("duplicate-artifactid-different-groupid"));
-        Model duplicateArtifactidPomModel = writeModel(duplicateArtifactidProjectDir.resolve("pom.xml").toFile(), new Model() {{
-            setModelVersion(pomModel.getModelVersion());
-            setParent(new Parent() {{
-                setGroupId(pomModel.getGroupId());
-                setArtifactId(pomModel.getArtifactId());
-                setVersion(pomModel.getVersion());
-            }});
-            setArtifactId("main");
         }});
 
         // When
@@ -268,12 +257,81 @@ class GitVersioningExtensionIT {
                     "git.ref"
             );
         }));
+    }
 
-        Model duplicateArtifactidGitVersionedPomModelLogic = readModel(duplicateArtifactidProjectDir.resolve("target/").resolve(GIT_VERSIONING_POM_NAME).toFile());
+    @Test
+    void commitVersioning_multiModuleProject_ambiguous_artifactId() throws Exception {
+        // Given
+        Git.init().setDirectory(projectDir.toFile()).call();
+
+        pomModel.setPackaging("pom");
+        pomModel.addModule("logic");
+        pomModel.addModule("another-group-logic");
+
+        writeModel(projectDir.resolve("pom.xml").toFile(), pomModel);
+        writeExtensionsFile(projectDir);
+        writeExtensionConfigFile(projectDir, extensionConfig);
+
+        Path logicProjectDir = Files.createDirectories(projectDir.resolve("logic"));
+        Model logicPomModel = writeModel(logicProjectDir.resolve("pom.xml").toFile(), new Model() {{
+            setModelVersion(pomModel.getModelVersion());
+            setParent(new Parent() {{
+                setGroupId(pomModel.getGroupId());
+                setArtifactId(pomModel.getArtifactId());
+                setVersion(pomModel.getVersion());
+            }});
+            setArtifactId("logic");
+        }});
+        Path anotherGroupLogicProjectDir = Files.createDirectories(projectDir.resolve("another-group-logic"));
+        Model anotherGroupLogicPomModel = writeModel(anotherGroupLogicProjectDir.resolve("pom.xml").toFile(), new Model() {{
+            setModelVersion(pomModel.getModelVersion());
+            setParent(new Parent() {{
+                setGroupId(pomModel.getGroupId());
+                setArtifactId(pomModel.getArtifactId());
+                setVersion(pomModel.getVersion());
+            }});
+            setGroupId("another.group");
+            setArtifactId("logic");
+        }});
+
+        // When
+        Verifier verifier = new Verifier(projectDir.toFile().getAbsolutePath());
+        verifier.executeGoal("verify");
+
+        // Then
+        String log = getLog(verifier);
+        assertThat(log).doesNotContain("[ERROR]", "[FATAL]");
+        String expectedVersion = NO_COMMIT;
+        assertThat(log).contains("Building " + pomModel.getArtifactId() + " " + expectedVersion);
+        Model gitVersionedPomModel = readModel(projectDir.resolve("target/").resolve(GIT_VERSIONING_POM_NAME).toFile());
+        assertThat(gitVersionedPomModel).satisfies(it -> assertSoftly(softly -> {
+            softly.assertThat(it.getModelVersion()).isEqualTo(pomModel.getModelVersion());
+            softly.assertThat(it.getGroupId()).isEqualTo(pomModel.getGroupId());
+            softly.assertThat(it.getArtifactId()).isEqualTo(pomModel.getArtifactId());
+            softly.assertThat(it.getVersion()).isEqualTo(expectedVersion);
+            softly.assertThat(it.getProperties()).doesNotContainKeys(
+                    "git.commit",
+                    "git.ref"
+            );
+        }));
+
+        Model apiGitVersionedPomModelLogic = readModel(logicProjectDir.resolve("target/").resolve(GIT_VERSIONING_POM_NAME).toFile());
+        assertThat(apiGitVersionedPomModelLogic).satisfies(it -> assertSoftly(softly -> {
+            softly.assertThat(it.getModelVersion()).isEqualTo(logicPomModel.getModelVersion());
+            softly.assertThat(it.getGroupId()).isEqualTo(logicPomModel.getGroupId());
+            softly.assertThat(it.getArtifactId()).isEqualTo(logicPomModel.getArtifactId());
+            softly.assertThat(it.getVersion()).isEqualTo(null);
+            softly.assertThat(it.getProperties()).doesNotContainKeys(
+                    "git.commit",
+                    "git.ref"
+            );
+        }));
+
+        Model duplicateArtifactidGitVersionedPomModelLogic = readModel(anotherGroupLogicProjectDir.resolve("target/").resolve(GIT_VERSIONING_POM_NAME).toFile());
         assertThat(duplicateArtifactidGitVersionedPomModelLogic).satisfies(it -> assertSoftly(softly -> {
-            softly.assertThat(it.getModelVersion()).isEqualTo(duplicateArtifactidPomModel.getModelVersion());
-            softly.assertThat(it.getGroupId()).isEqualTo(duplicateArtifactidPomModel.getGroupId());
-            softly.assertThat(it.getArtifactId()).isEqualTo(duplicateArtifactidPomModel.getArtifactId());
+            softly.assertThat(it.getModelVersion()).isEqualTo(anotherGroupLogicPomModel.getModelVersion());
+            softly.assertThat(it.getGroupId()).isEqualTo(anotherGroupLogicPomModel.getGroupId());
+            softly.assertThat(it.getArtifactId()).isEqualTo(anotherGroupLogicPomModel.getArtifactId());
             softly.assertThat(it.getVersion()).isEqualTo(null);
             softly.assertThat(it.getProperties()).doesNotContainKeys(
                     "git.commit",
@@ -285,7 +343,6 @@ class GitVersioningExtensionIT {
     @Test
     void branchVersioning_multiModuleProject_noParent() throws Exception {
         // Given
-
         try (Git git = Git.init().setDirectory(projectDir.toFile()).call()) {
             RevCommit givenCommit = git.commit().setMessage("initial commit").setAllowEmpty(true).call();
             String givenBranch = "feature/test";
