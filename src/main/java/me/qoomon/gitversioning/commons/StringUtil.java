9ec8dc9426eaedfcdc1789ce1a6056c3f2206aaa
Bengt Brodersen <bengt.brodersen@gmail.com>
2021-07-15 09:05:58 +0200

    fix: add index groups to patternGroups function result

--- src/main/java/me/qoomon/gitversioning/commons/StringUtil.java
+++ src/main/java/me/qoomon/gitversioning/commons/StringUtil.java
@@ -52,30 +52,51 @@ public final class StringUtil {
      */
     public static Map<String, String> patternGroupValues(Pattern pattern, String text) {
         Map<String, String> result = new HashMap<>();
+
         Matcher groupMatcher = pattern.matcher(text);
         if (groupMatcher.find()) {
-            // add group index to value entries
+            // group index values
             for (int i = 1; i <= groupMatcher.groupCount(); i++) {
                 result.put(String.valueOf(i), groupMatcher.group(i));
             }
-
-            for (String groupName : patternGroups(pattern)) {
+            // group name values
+            for (String groupName : patternGroupNames(pattern)) {
                 result.put(groupName, groupMatcher.group(groupName));
             }
         }
+
         return result;
     }
 
-    public static Set<String> patternGroups(Pattern groupPattern) {
-        Set<String> groupNames = new HashSet<>();
-        Pattern groupNamePattern = Pattern.compile("\\(\\?<(?<name>[a-zA-Z][a-zA-Z0-9]*)>");
-        Matcher groupNameMatcher = groupNamePattern.matcher(groupPattern.toString());
+    public static Set<String> patternGroups(Pattern pattern) {
+        Set<String> groups = new HashSet<>();
 
-        // add group name to value Entries
+        // group indexes
+        for (int groupIndex = 1; groupIndex <= patternGroupCount(pattern); groupIndex++) {
+            groups.add(String.valueOf(groupIndex));
+        }
+        // group names
+        groups.addAll(patternGroupNames(pattern));
+
+        return groups;
+    }
+
+    public static int patternGroupCount(Pattern pattern) {
+        return pattern.matcher("").groupCount();
+    }
+
+    public static Set<String> patternGroupNames(Pattern pattern) {
+        Set<String> groups = new HashSet<>();
+
+        // group names
+        Pattern groupNamePattern = Pattern.compile("\\(\\?<(?<name>[a-zA-Z][a-zA-Z0-9]*)>");
+        Matcher groupNameMatcher = groupNamePattern.matcher(pattern.toString());
         while (groupNameMatcher.find()) {
             String groupName = groupNameMatcher.group("name");
-            groupNames.add(groupName);
+            groups.add(groupName);
         }
-        return groupNames;
+
+        return groups;
     }
+
 }
