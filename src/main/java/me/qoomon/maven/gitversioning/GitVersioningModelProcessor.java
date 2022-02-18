38d5db97d0d3f653144888d00f4d07908aa06083
tkky5 <philipp.kyas@six-group.com>
2022-02-18 20:10:18 +0100

    escape dot

--- src/main/java/me/qoomon/maven/gitversioning/GitVersioningModelProcessor.java
+++ src/main/java/me/qoomon/maven/gitversioning/GitVersioningModelProcessor.java
@@ -705,7 +705,7 @@ public class GitVersioningModelProcessor extends DefaultModelProcessor {
         placeholderMap.put("version", originalProjectVersion);
         placeholderMap.put("version.release", Lazy.by(() -> originalProjectVersion.get().replaceFirst("-SNAPSHOT$", "")));
 
-        String[] versionComponents = originalProjectVersion.get().replaceFirst("-.*$","").split(".");
+        String[] versionComponents = originalProjectVersion.get().replaceFirst("-.*$","").split("\\.");
         placeholderMap.put("version.major", Lazy.by(() -> versionComponents.length >= 1 ? versionComponents[0] : ""));
         placeholderMap.put("version.minor", Lazy.by(() -> versionComponents.length >= 2 ? versionComponents[1] : ""));
         placeholderMap.put("version.patch", Lazy.by(() -> versionComponents.length >= 3 ? versionComponents[2] : ""));
